2ad790c82546594a59b69ac80b54799a1daf3a7c
itz-abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2025-10-15 09:00:39 +0100

    make products fetch from live render site

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
+  server: {
     proxy: {
-      '/api': {
-        target: `http://localhost:3124`,
+      "/api": {
+        target: `https://male-fashion-1ihm.onrender.com`,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
